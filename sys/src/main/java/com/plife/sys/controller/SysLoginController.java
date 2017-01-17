8502976a40e587c83406e1ad75ab8087ec7a484d
jamen <chenjn@coracle.com>
2017-01-17 14:58:03 +0800

    boostrapt 模板导入。

---
+++ sys/src/main/java/com/plife/sys/controller/SysLoginController.java
@@ -0,0 +1,13 @@
+package com.plife.sys.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * Created by chenjianan on 2017/1/17-14:07.
+ * <p>
+ * Describe: 系统登录 controller
+ */
+@RequestMapping("sys/login")
+public class SysLoginController {
+
+}
